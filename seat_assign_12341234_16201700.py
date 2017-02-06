cf130a0e0b83b699cbcd3803e6f9cb4c598064f0
ARI Assignment <ying.wang@ucdconnect.ie>
2017-02-06 15:12:08 +0000

    Committed by Ying Wang
    This is an immature idea for the first section of coding. Needs to update later.

--- seat_assign_12341234_16201700.py
+++ seat_assign_12341234_16201700.py
@@ -1,10 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 22 15:50:32 2017
+@authors: Niall Daly and Ying Wang
+import random
+class Booking:
+    def __init_self_():
+        pass
+    def get_avail_seats(c, row_num):
+        row_query = "select instr(seats, seat) from rows_cols join seating where row = ? and name = '' order by 1 asc;"
+        return c.execute(row_query, [row_num]).fetchall()
 
-@authors: Niall and Tracy
-"""
-
-def get_avail_seats(c, row_num):
-    row_query = "select instr(seats, seat) from rows_cols join seating where row = ? and name = '' order by 1 asc;"
-    return c.execute(row_query, [row_num]).fetchall()
+    def get_consec_avail(c, avail, seats):
+        max_consec_seats = avail[0]
+        cur_consec_seats = avail[0]
+        for i in range(0, len(avail)-1):
+            if avail[i][0] + 1 != avail[i+1][0]: 
+                cur_consec_seats = avail[i+1]
+            else:
+                cur_consec_seats = cur_consec_seats + avail[i+1] 
+        for neighbour in max_consec_seats:
+            consec+=seats[neighbour-1]
+        return consec
